08b07daff79404eb00c737d70dda8eee2c93f6c8
Enrique-Mora-Zavala <L21390359@chetumal.tecnm.mx>
2026-06-05 07:55:24 -0500

    Se installa axios para obtener datos del JSON Server y se crea el componente Post.vue

---
+++ src/components/Post.vue
@@ -0,0 +1,11 @@
+<template>
+  <tr>
+    <td>1</td>
+    <td>Hello World!</td>
+    <td>
+      <button class="btn btn-green">O</button>
+      <button class="btn btn-blue">/</button>
+      <button class="btn btn-red">X</button>
+    </td>
+  </tr>
+</template>
