dfe7f35ea254a3c1c0ab4a41340ecf3c3ea6288c
Enrique-Mora-Zavala <L21390359@chetumal.tecnm.mx>
2026-06-05 08:02:05 -0500

    Se empieza a crear un estado reactivo para obtener los datos de JSON Server, empezando por modificar el componente Posts.vue

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,12 +1,27 @@
 <script setup>
+import { onMounted, reactive } from 'vue'
 import { RouterLink } from 'vue-router'
+import axios from 'axios'
 import Post from '@/components/Post.vue'
-</script>
 
+const state = reactive({
+  posts: [],
+})
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('http://localhost:8080/posts')
+    state.posts = response.data
+  } catch (error) {
+    console.error('Error fetching jobs:', error)
+  }
+})
+</script>
 <template>
   <div class="center-container">
     <h1>All Post</h1>
     <RouterLink :to="`/`" class="btn btn-blue">Go to Home</RouterLink>
+    <RouterLink :to="`/posts/add`" class="btn btn-green">Add Post</RouterLink>
     <br />
     <br />
     <table id="posts">
@@ -15,7 +30,7 @@ import Post from '@/components/Post.vue'
         <th>Title</th>
         <th>Action</th>
       </tr>
-      <Post />
+      <Post v-for="post in state.posts" :key="post.id" :post="post" />
     </table>
   </div>
 </template>
